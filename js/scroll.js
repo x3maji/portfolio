14fa8f409162c3e37dd04f1b6f7cf72df25cc9dd
Дмитрий <57167689+x3maji@users.noreply.github.com>
2019-12-08 11:21:11 +0300

    Add files via upload

---
+++ js/scroll.js
@@ -0,0 +1,9 @@
+jQuery(document).ready(function($) {
+var $page = $('html, body');
+$('a[href*="#"]').click(function() {
+    $page.animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 400);
+    return false;
+});});
+
